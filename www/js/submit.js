3c57e7dc90090328a6af9bccf551c0ef798ddcb0
Marcin <dlugopix@gmail.com>
2016-01-08 22:07:30 +0100

    Test project cordova

--- www/js/submit.js
+++ www/js/submit.js
@@ -5,6 +5,10 @@
  * Created by Marcin on 21.12.2015.
  */
 document.addEventListener('DOMContentLoaded', function() {
+    var action_g = function (start, end) {
+        return "http://edi.iem.pw.edu.pl/matlaczm/apka/map/" + start + "/" + end;
+    };
+
     document.getElementById("start").onkeypress = function (event) {
         if (event.which == 13 || event.keyCode == 13) {
             event.preventDefault();
@@ -17,13 +21,43 @@ document.addEventListener('DOMContentLoaded', function() {
     };
     document.getElementById("start").onblur = function () {
         start = document.getElementById("start").value;
-        document.getElementsByTagName("form")[0].action = "http://edi.iem.pw.edu.pl/matlaczm/apka/map/" + start + "/" + end;
+        document.getElementsByTagName("form")[0].action = action_g(start, end);
         console.log("start")
     };
     document.getElementById("end").onblur = function () {
         end = document.getElementById("end").value;
-        document.getElementsByTagName("form")[0].action = "http://edi.iem.pw.edu.pl/matlaczm/apka/map/" + start + "/" + end;
+        document.getElementsByTagName("form")[0].action = action_g(start, end);
         console.log("end")
     };
-
+    document.getElementById("submit").onclick = function (event) {
+        event.preventDefault();
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                var data = $.parseJSON(xhttp.responseText);
+                console.log(data);
+                var container = $('.typeahead-demo');
+                container.children().remove();
+                container.append("<pre>Start: "+data['start']+"</pre> <pre>Koniec: "+data['finish']+"</pre>");
+                for (i in data['path']){
+                    container.append("<pre>"+i+". "+data['path'][i]+"</pre>");
+                }
+                container.append("<pre>"+data['distance']+" KM</pre>");
+                container.append("<button type='reset' onclick='location.reload()'>Back</button>");
+            }
+        };
+        xhttp.open("GET", action_g(start, end), true);
+        xhttp.send();
+        console.log(xhttp.responseText)
+    }
 });
+
+
+
+
+
+
+
+
+
+
