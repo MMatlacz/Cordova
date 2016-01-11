0d037ca1a838239b541201c289a561c255c8852a
Marcin <dlugopix@gmail.com>
2016-01-11 14:30:08 +0100

    added map

--- www/js/submit.js
+++ www/js/submit.js
@@ -4,7 +4,7 @@
 /**
  * Created by Marcin on 21.12.2015.
  */
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var action_g = function (start, end) {
         return "http://edi.iem.pw.edu.pl/matlaczm/apka/map/" + start + "/" + end;
     };
@@ -36,14 +36,18 @@ document.addEventListener('DOMContentLoaded', function() {
             if (xhttp.readyState == 4 && xhttp.status == 200) {
                 var data = $.parseJSON(xhttp.responseText);
                 console.log(data);
-                var container = $('.typeahead-demo');
+                var container = $('.typeahead');
                 container.children().remove();
-                container.append("<pre>Start: "+data['start']+"</pre> <pre>Koniec: "+data['finish']+"</pre>");
-                for (i in data['path']){
-                    container.append("<pre>"+i+". "+data['path'][i]+"</pre>");
+                container.append("<pre>Start: " + data['start'] + "</pre> <pre>Koniec: " + data['finish'] + "</pre>");
+                for (i in data['path']) {
+                    container.append("<pre>" + i + ". " + data['path'][i] + "</pre>");
                 }
-                container.append("<pre>"+data['distance']+" KM</pre>");
-                container.append("<button type='reset' onclick='location.reload()'>Back</button>");
+                container.append("<pre>" + data['distance'] + " KM</pre>");
+                container.append("<div id='map'></div>");
+                $('body').append("<button type='reset' onclick='location.reload()'>Back</button>");
+                initMap();
+                mapa(data['path']);
+
             }
         };
         xhttp.open("GET", action_g(start, end), true);
@@ -51,7 +55,46 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log(xhttp.responseText)
     }
 });
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 52.276714, lng: 19.470538},
+        zoom: 6
+    });
+}
+function mapa(path){
+    var planCoordinates = [];
+    for(i in path) {
+        console.log(path[i]);
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                var data = $.parseJSON(xhttp.responseText);
+                console.log(data);
+                var loc = data['results'][0]['geometry']['location'];
+                planCoordinates.push(loc);
+                new google.maps.Marker({
+                    position: loc,
+                    map: map,
+                    title: 'Hello World!'
+                });
 
+
+            }
+        };
+        xhttp.open("GET", 'https://maps.googleapis.com/maps/api/geocode/json?address='+path[i]+'&key=AIzaSyC0fyRGllwPtxZdknHd_nPYAuloCYIiF_0'
+            , false);
+        xhttp.send();
+    }
+    var newPath = new google.maps.Polyline({
+        path: planCoordinates,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+    newPath.setMap(map);
+}
 
 
 
